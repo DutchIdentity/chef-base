152b6e1665453e5a918494b8163944f85a720fd2
Daniel Paulus <d.paulus@gmail.com>
2014-12-16 19:43:41 +0100

    Add basic fail2ban support

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'd.paulus@gmail.com'
 license 'Apache v2.0'
 description 'Installs/Configures Dutch Identity Base Config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.3'
+version '0.0.4'
 
 depends 'postfix', '3.6.0'
 depends 'users', '1.7.1'
 depends 'sudo', '2.7.1'
 depends 'openssh', '1.3.5'
+depends 'fail2ban', '2.2.1'
